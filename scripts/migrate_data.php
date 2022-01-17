f8f45aabd594bc2d45c172cded332765d0bfd945
Sem van Lent <sl@improvement-it.nl>
2022-01-17 22:40:10 +0100

    Import data from users text file

--- scripts/migrate_data.php
+++ scripts/migrate_data.php
@@ -5,11 +5,38 @@ require_once './query_pdo.php';
 if (file_exists('../storage/users.txt')) {
     $userFile = fopen('../storage/users.txt', 'r');
     while (!feof($userFile)) {
-        $users[] = explode('|', fgets($userFile));        
+        $users[] = explode('|', trim(fgets($userFile)));
     }
     array_shift($users);
 
+    $processed = 0;
+    $insertValues = '';
     foreach ($users as $user) {
+        $user[] = createId(6);
+        $user[] = 1;
         print_r($user);
+        $processed++;
+        $imploded = "('" . implode("', '", $user) . "')" . ($processed < count($users) ? ", " : ";");
+        echo $imploded . PHP_EOL;
+        $insertValues .= $imploded;
     }
+}
+
+createQuery($insertValues);
+
+function createQuery($values) {
+    $insert = "INSERT INTO users (email, username, pass, id, reg) VALUES " . $values;
+    qpdo($insert);
+}
+
+function createId($length)
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $character = substr($characters, rand(0, strlen($characters) - 1), 1);
+        $randomString .= $character;
+    }
+    return $randomString;
 }
