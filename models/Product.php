f7698ce7eada2ad8bff53e9d470b32db1f706e43
Sem van Lent <sl@improvement-it.nl>
2022-02-10 00:40:26 +0100

    Start product model

---
+++ models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once ROOT . '/classes/DB.php';
+require_once ROOT . '/classes/ID.php';
+
+class Product extends DB
+{
+    public static function getProductList()
+    {
+        $query = self::query("SELECT `id`, `name`, 'price', `image` FROM products WHERE show='1'");
+        $result = [];
+
+        while ($row = $query->fetch()) {
+            $result[] = $row;
+        }
+
+        return $result;
+    }
+}
