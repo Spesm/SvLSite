693ccf44562e48042115fb84e0ebbf102cab5631
Sem van Lent <sl@improvement-it.nl>
2022-02-22 02:51:50 +0100

    Make product price tag

--- views/webshop_content.php
+++ views/webshop_content.php
@@ -5,12 +5,12 @@ require_once ROOT . '/models/product.php';
 function showWebshopContent($render = true)
 {
     $products = Product::getProducts();
-    print_r($products);
+    // print_r($products);
 
     if ($render) : ?>
         <div class="content">
-            <h1 class="product_header">Offers you can't refuse</h1>
-            <div class="product_index">
+            <h1 class="product-header">Offers you can't refuse</h1>
+            <div class="product-index">
                 <?php foreach ($products as $product) {
                     showProductCard($product);
                 } ?>
@@ -19,21 +19,41 @@ function showWebshopContent($render = true)
     <?php endif;
 }
 
-function formatPrice($price) {
-    echo strval($price);
-    $cents = strpos($price, '.') ? substr($price, strpos($price, '.') + 1, 2) : '';
-    echo nl2br("\n" . $cents);
+function formatEuros($price)
+{
     $euros = strpos($price, '.') ? substr($price, 0, strpos($price, '.')) : $price;
-    echo nl2br("\n" . $euros);
+
+    if (strlen($euros) > 3) {
+        $n = 0;
+        $figure = '';
+        $digits = str_split(strrev($euros));
+        foreach ($digits as $digit) {
+            $figure .= $n % 3 === 0 ? '.' . $digit : $digit;
+            $n++;
+        }
+        $euros = substr(strrev($figure), 0, strlen($figure) - 1);
+    }
+
+    return '&euro; ' . $euros . ',';
+}
+
+function formatCents($price)
+{
+    $cents = strpos($price, '.') ? substr($price, strpos($price, '.') + 1, 2) : '00';
+
+    return $cents;
 }
 
 function showProductCard($product)
 {
     if ($product['id']) : ?>
-        <div class="product_card">
-            <h2 class="product_name"><?php echo $product['name'] ?></h2>
+        <div class="product-card">
+            <h2 class="product-name"><?php echo $product['name'] ?></h2>
             <img src="<?php echo HOME . '/storage/images/products/' . $product['image'] ?>">
-            <h2 class="product_price"><?php formatPrice($product['price']) ?></h2>
+            <span class="price-tag">
+                <h2 class="euros"><?php echo formatEuros($product['price']) ?></h2>
+                <h4 class="cents"><?php echo formatCents($product['price']) ?></h4>
+            </span>
         </div>
-    <?php endif;
+<?php endif;
 }
