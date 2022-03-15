ab484b13a6aeb02fe61e1748b5b8cb2ebbf7f2d5
Sem van Lent <sl@improvement-it.nl>
2022-03-15 01:50:00 +0100

    Increment and decrement cart item amounts

--- javascripts/jquery_functions.js
+++ javascripts/jquery_functions.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
-    $('.add-product-button').click(function(){
+
+    $('.add-product-button').click(function() {
         $('.product-count').text((_, present) => parseInt(present) + 1)
         $.post('http://localhost/SvLSite/index.php', {productId: this.id})
+    })
+
+    $('.item-amount .decrement').click(function() {
+        id = this.id.substring(4)
+        $('#num-' + id).val(function(_, present) {
+            if (present > 1) {                
+                return parseInt(present) - 1
+            } else {
+                return 0
+            }
+        })
+    })
+
+    $('.item-amount .increment').click(function() {
+        id = this.id.substring(4)
+        $('#num-' + id).val(function(_, present) {
+            if (present < 999) {                
+                return parseInt(present) + 1
+            } else {
+                return 999
+            }
+        })
     })
 })
